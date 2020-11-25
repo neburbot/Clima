796011f7017ccfd4df3a255d795dae3b6409a76b
IL <lob_alo@outlook.com>
2020-11-24 22:58:40 -0600

    Primer commit

---
+++ Clima/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Mac19 on 24/11/20.
+//  Copyright © 2020 ITM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
